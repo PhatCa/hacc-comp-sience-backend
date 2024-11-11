cf0642d10d315e2b93f21cbc17264733bb349995
PhatCa <phatca@hawaii.edu>
2024-11-10 14:01:34 -1000

    added url

--- appliances/models.py
+++ appliances/models.py
@@ -7,7 +7,8 @@ class Appliance(models.Model):
     name = models.CharField(max_length=200)
     power_usage = models.FloatField()
     is_on = models.BooleanField(default=False)
-
+    image_url = models.URLField(max_length=200, blank=True, null=True)
+    
     def __str__(self):
         return self.name
     

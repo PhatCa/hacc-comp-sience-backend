0217ee2d0088c30c0df3e5216c03334e68b37bd9
PhatCa <phatca@hawaii.edu>
2024-11-10 14:14:56 -1000

    tried to fix css

--- api/settings.py
+++ api/settings.py
@@ -143,6 +143,8 @@ STATICFILES_DIR = [
     BASE_DIR / 'static/icon',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
